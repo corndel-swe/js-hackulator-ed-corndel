faca160d3360d42bfd881dc0cb80522e8ece0aa8
Ed Gilmore <ed.gilmore@corndel.com>
2024-09-04 12:18:04 +0100

    maxOfThree: simplified function to use Math.max

--- src/maxOfThree.js
+++ src/maxOfThree.js
@@ -1,15 +1,5 @@
 function maxOfThree(x, y, z) {
-  if (x > y && x > z) {
-    return x
-  }
-
-  if (y > z && y > x) {
-    return y
-  }
-
-  if (z > x && z > y) {
-    return z
-  }
+  return Math.max(x, y, z)
 }
 
 export default maxOfThree
