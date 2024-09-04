a6cc3d2bc3eb2ed9b1d25374fbab513b8ae8ecd3
Ed Gilmore <ed.gilmore@corndel.com>
2024-09-04 12:23:57 +0100

    heron: defined s and area as constants to calculate semiperimeter and then area using Heron's formula

--- src/heron.js
+++ src/heron.js
@@ -4,7 +4,9 @@
  * https://en.wikipedia.org/wiki/Heron%27s_formula
  */
 function heron(a, b, c) {
-  return Math.sqrt(/*🤷*/)
+  const s = (a + b + c) / 2
+  const area = Math.sqrt(s*(s-a)*(s-b)*(s-c))
+  return area
 }
 
 export default heron
